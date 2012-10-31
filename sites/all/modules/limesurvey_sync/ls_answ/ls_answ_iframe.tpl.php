6afa1ecf594424e00361fb1816b831c893d72af0
recoverym <recovery.m@gmail.com>
2012-10-31 10:33:53 +0100

    versione al 31.10.2012

---
+++ sites/all/modules/limesurvey_sync/ls_answ/ls_answ_iframe.tpl.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @file
+ * Template opening an iframe.
+ */
+?>
+<div class="iframe" name="ls_form" id="ls_form">
+  <iframe src="<?php print $url ?>" width="<?php print $width ?>" height="<?php print $height ?>" frameborder="0" align="left"></iframe>
+</div>
+
+
